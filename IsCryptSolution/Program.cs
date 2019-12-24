5852f696c6e42706278643004fb8c19211a8166a
Vladimir Gasic <vladimir.gasic@gemikro.rs>
2019-12-24 14:40:47 +0100

    IsCryptSolution setup

---
+++ IsCryptSolution/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IsCryptSolution {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] crypt = new[] { "SEND", "MORE", "MONEY" };
+
+            char[][] solution = new[] {
+                new[] {'O', '0' },
+                new[] {'M', '1'},
+                new[] {'Y', '2'},
+                new[] {'E', '5'},
+                new[] {'N', '6'},
+                new[] {'D', '7'},
+                new[] {'R', '8'},
+                new[] {'S', '9'}
+            };
+
+        }
+
+
+        static bool isCryptSolution(string[] crypt, char[][] solution) {
+
+
+
+        }
+    }
+}
