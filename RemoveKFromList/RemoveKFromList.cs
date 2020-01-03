83472bde95bc71a5f43d99755986d1c2e46db807
Vladimir Gasic <vladimir.gasic@gemikro.rs>
2020-01-03 16:10:01 +0100

    messing with recursion

--- RemoveKFromList/RemoveKFromList.cs
+++ RemoveKFromList/RemoveKFromList.cs
@@ -1,6 +1,21 @@
 ﻿using System;
 
 namespace RemoveKFromList {
+
+    /*
+    Note: Try to solve this task in O(n) time using O(1) additional space,
+    where n is the number of elements in the list, since this is what you'll be asked to do during an interview.
+
+    Given a singly linked list of integers l and an integer k, remove all elements from list l that have a value equal to k.
+
+    Example
+
+    For l = [3, 1, 2, 3, 4, 5] and k = 3, the output should be
+    removeKFromList(l, k) = [1, 2, 4, 5];
+    For l = [1, 2, 3, 4, 5, 6, 7] and k = 10, the output should be
+    removeKFromList(l, k) = [1, 2, 3, 4, 5, 6, 7].
+    */
+
     class RemoveKFromList {
         static void Main(string[] args) {
 
@@ -8,26 +23,28 @@ namespace RemoveKFromList {
 
             var linkedList = LinkedListFromArray(arr);
 
+            removeKFromList(linkedList, 3);
+
+            //Console.ReadLine();
+
         }
 
 
         static ListNode<int> LinkedListFromArray(int[] arr) {
 
-            ListNode<int> res = null;
+            // add first node
+            ListNode<int> res = new ListNode<int>() { value = arr[0], next = null };
 
-            for (int i = 0; i < arr.Length; i++) {
-                res = InsertNode(res, arr[i]);
+            // skip first because it's already added
+            for (int i = 1; i < arr.Length; i++) {
+                res.InsertNodeToEnd(res, arr[i]);
             }
 
             return res;
         }
 
 
-        static ListNode<int> InsertNode(ListNode<int>, int value) {
 
-            ListNode<int> tmpNode = new ListNode<int>();
-
-        }
 
 
         // Singly-linked lists are already defined with this interface:
@@ -36,16 +53,50 @@ namespace RemoveKFromList {
         //   public ListNode<T> next { get; set; }
         // }
         //
-        ListNode<int> removeKFromList(ListNode<int> l, int k) {
+        static ListNode<int> removeKFromList(ListNode<int> l, int k) {
 
-            throw new NotImplementedException();
+            
+
+
 
         }
     }
 
 
-    class ListNode<T> {
+    public class ListNode<T> {
+
         public T value { get; set; }
+
         public ListNode<T> next { get; set; }
+
+
+        #region methods
+
+        public ListNode<T> InsertNodeToEnd(ListNode<T> root, T value) {
+
+            ListNode<T> tmpNode = new ListNode<T>();
+            tmpNode.value = value;
+            tmpNode.next = null;
+
+            ListNode<T> ptr;
+
+            // if root is empty
+            if (root == null) {
+                root = tmpNode;
+            } else {
+                ptr = root;
+
+                while (ptr.next != null) {
+                    ptr = ptr.next;
+                }
+
+                ptr.next = tmpNode;
+            }
+
+            return root;
+
+        }
+
+        #endregion
     }
 }
